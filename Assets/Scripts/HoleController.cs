f315e25e5fb2d51c5401ef86bd4169e6d3b97b49
Derek Sliman <Laito.Lawiet@gmail.com>
2020-04-22 20:34:16 +0300

    Time and scene controllers

---
+++ Assets/Scripts/HoleController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleController : MonoBehaviour
+{
+    [HideInInspector] public bool isActive;
+
+    public void CreateHole()
+    {
+        // Creating hole, set color and activate
+
+        isActive = true;
+    }
+
+    public void DisableHole(bool isWin)
+    {
+
+    }
+}
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,18 +1,79 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region PARAMS
+
+    [Header("Links")]
+    [SerializeField] public GameObject ruleteObj;
+
+    [SerializeField] private Text _timerText;
+    [SerializeField] private Text _scoreText;
+
+    [Header("Params")]
+    [SerializeField] private float _timeForMatch;
+    [SerializeField] private float _ruleteSpeed;
+    [SerializeField] private float _scoreModifide;
+
+    #endregion
+
+    #region PRIVATE
+
+    private bool _isPlayGame;
+    private float _playerScore;
+
+    #endregion
+
+    #region METHODS
+
+    private void Start()
     {
-        
+        _isPlayGame = true;
+        _playerScore = 0;
+        GenerateWorld();
+        StartCoroutine(UpdateTimeUI());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_isPlayGame)
+        {
+            ruleteObj.transform.Rotate(new Vector3(0, 0, 1), _ruleteSpeed);
+        }
     }
+
+    private void GenerateWorld()
+    {
+        for (int holeId = 0; holeId < ruleteObj.transform.childCount; holeId++) ruleteObj.transform.GetChild(holeId).GetComponent<HoleController>().CreateHole();
+    }
+
+    private void GameEnd()
+    {
+        Debug.LogError("Game End");
+
+        _isPlayGame = false;
+
+        for (int holeId = 0; holeId < ruleteObj.transform.childCount; holeId++)
+        {
+            if (!ruleteObj.transform.GetChild(holeId).GetComponent<HoleController>().isActive) _playerScore += 1 * _scoreModifide;
+        }
+
+        _scoreText.text = "Score : " + _playerScore;
+    }
+
+    private IEnumerator UpdateTimeUI()
+    {
+        while (_timeForMatch > 0)
+        {
+            _timerText.text = "Timer : " + _timeForMatch;
+            yield return new WaitForSecondsRealtime(1f);
+            _timeForMatch -= 1;
+        }
+
+        GameEnd();
+    }
+
+    #endregion
 }
